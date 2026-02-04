f42153fa569d0eb417d4b394c37353bbb56a25cc
Ruslan <lelyimalikrusik12301@gmail.com>
2026-02-04 22:55:18 +0300

    df

--- InvestBackend/main.py
+++ InvestBackend/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
